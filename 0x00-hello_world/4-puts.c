987acd9b94a0189adcd9c55fc3f9f1d1dd1d5a3a
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-15 22:49:12 +0000

    Hello puts, a c program that uses puts to print a stream of characters

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ * 
+ * Return: always 0(Success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
+}
