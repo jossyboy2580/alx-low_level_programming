90e1e07188d35443639d98333e5836daaa6498e2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-15 23:55:51 +0000

    What are the sizes of all these files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %d", sizeof(int));
 	printf("Size of a long int: %d", sizeof(long int));
 	printf("Size of a long long: %d", sizeof(long long));
-	printf("Size of a long float: %d", sizeof(long float);
+	printf("Size of a long float: %d", sizeof(long float));
 	return (0);
 }
