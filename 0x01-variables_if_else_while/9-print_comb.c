13f4d730e69e8056f83cd818d5101a3a50b0c39b
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 13:52:00 +0000

    print single digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 48)
+			putchar(' ');
+		putchar(i);
+		if (i != 57)
+			putchar(',');
+		putchar('\n');
+		return (0);
+	}
+}
