0d027ae54a01902a16cf4c86571729ee504cc852
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 13:10:41 +0000

    A c program to print all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
