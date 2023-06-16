e94b6096bab6e533d34fc8d1b3150273f41db28e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 19:41:43 +0000

    Print a combination of double digit integers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 8; i++)
+	for (i = 48; i <= 56; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = 49; j <= 57; j++)
 		{
 			if (i < j)
 			{
-				if (i != 0 && j != 1)
+				if (i != 48 && j != 49)
 					putchar(' ');
 				putchar(i);
 				putchar(j);
-				if (i != 8 && j != 9)
+				if (i != 56 && j != 57)
 					putchar(',');
 			}
 		}
