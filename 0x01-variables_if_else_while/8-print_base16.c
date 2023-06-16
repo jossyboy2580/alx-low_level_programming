38db9284b4fe986d100d8bda770c2d41db9492c4
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 13:34:23 +0000

    This c code prints the base 16 numerals in ascending order in lower case

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
