03c6f3a9fb9212df01c4ce31ebc76eed7921b53a
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 17:07:45 -0700

    Print out the different possible combination of three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if ( i < j && j < k && i < k)
+				{
+					if (i + j + k > 147)
+						putchar(' ');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j + k < 168)
+						putchar(',');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
