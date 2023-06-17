14511ccf095fe251c7702be34f21dbe94325e955
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-16 18:20:35 -0700

    Lets print the last digit of this random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,5 +19,11 @@ int main(void)
 			n / 10;
 		}
 		printf("%d", n);
+		if (n > 5)
+			printf("and is greater than 5\n");
+		if (n < 6 && n != 0)
+			printf("and is less than 6 and not 0\n");
+		if (n == 0)
+			printf("and is 0\n");
 		return (0);
 }
