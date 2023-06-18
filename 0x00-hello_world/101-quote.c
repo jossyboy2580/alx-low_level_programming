1d3fd0a050508dde561ed8b7bd8eb5b11de51769
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-17 23:48:56 -0700

    This program uses the perror function to print to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * maain - The entry point of a program that prints to
+ * the standard error without using functions like fprintf
+ * fputs etc
+ *
+ * Return: 1(success)
+ */
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
