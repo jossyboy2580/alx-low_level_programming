a156f6f34f6ff9ea84255b82c67f62b8fd757f5e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 15:05:27 +0000

    Print putchar to the standard output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - The entry point to a program that prints the character putchar
- * using the _putchar function
+ * main - The entry point of the program
  *
  * Return: always 0(success)
  */
 
 int main(void)
 {
-	int i = 97;
-
-	while (i < 123)
-	{
-		_putchar(i);
-		i++;
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - The entry point to a program that prints all lower case letters
+ * using the _putchar function
+ *
+ * Return: always 0(success)
+ */
+
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return;
+}
