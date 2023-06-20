b6ccc0e92d0fe7f9f1de141334ba3691966b11a2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 22:25:37 +0000

    This jack bauer task gave me a scare but thank God i was able to solve it

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void disp_min(int, int);
+
 /**
  * jack_bauer - This function prints the total time
  * jack bauer has to solve a crime
@@ -7,24 +9,39 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 10; j++)
 		{
-			for (k = 0; k < 6; k++)
-			{
-				for (l = 0; l < 10; l++)
-				{
-					_putchar(i+'0');
-					_putchar(j+'0');
-					_putchar(':');
-					_putchar(k+'0');
-					_putchar(l+'0');
-					_putchar('\n');
-				}
-			}
+			if ((i < 2) || (i == 2 && j < 4))
+				disp_min(i, j);
+		}
+	}
+}
+
+/**
+ * disp_min - Display the minutes
+ *
+ * @hr1: first hand of the hour
+ * @hr2: second part of the hour
+ */
+
+void disp_min(int hr1, int hr2)
+{
+	int k, l;
+
+	for (k = 0; k < 6; k++)
+	{
+		for (l = 0; l < 10; l++)
+		{
+			_putchar(hr1 + '0');
+			_putchar(hr2 + '0');
+			_putchar(':');
+			_putchar(k + '0');
+			_putchar(l + '0');
+			_putchar('\n');
 		}
 	}
 }
