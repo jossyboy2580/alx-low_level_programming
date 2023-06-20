468e5c6dcdb29e2cf017f79ce971834a3ecb5135
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 19:20:42 +0000

    The jack bauer time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - This function prints the total time
+ * jack bauer has to solve a crime
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 6; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					_putchar(i+'0');
+					_putchar(j+'0');
+					_putchar(':');
+					_putchar(k+'0');
+					_putchar(l+'0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
