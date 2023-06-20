cf1347975f645578fee019933c2e8de9baa4bf90
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 23:36:55 +0000

    Print all natural numbers between n and 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ *
+ * @n: The number to start the count from
+ */
+
+void print_to_98(int n)
+{
+	printf("%d" n);
+	if (n > 98)
+	{
+		int start = n;
+		while (start >= 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+			printf("%d", start--);
+		}
+	}
+	else if (n < 98)
+	{
+		int start = n;
+		while (start <= 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+			printf("%d", start++);
+		}
+	}
+	_putchar('\n');
+}
