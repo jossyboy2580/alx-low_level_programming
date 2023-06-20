106db46f9f1062fcdacfc77d51da0431306424a8
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 23:08:37 +0000

    Calculate the 9 times table and putchar it

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Function to print the times table from
+ * 0 to 9
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1; j < 10; j++)
+		{
+			int prod;
+
+			prod = i * j;
+			if (prod < 10)
+				_putchar(' ');
+			_putchar(' ');
+			if (prod / 10)
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+			if (j != 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
