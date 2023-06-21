87f4997bec3387b698a0fbb7a1675f3d99fc3313
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-21 03:03:14 -0700

    Debugging for zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - A test function to check if a given number
+ * is positive or negative
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
