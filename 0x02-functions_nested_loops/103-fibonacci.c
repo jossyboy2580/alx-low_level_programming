d0861721a21e36ad46fc00ace5709c9b4b86b1a1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-20 18:32:40 -0700

    Print the sum of all even fibonacci numbers whose value does not exceed 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program that calculates the sum of all
+ * evem terms of the fibonacci sequence
+ * below 4,000,000
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	unsigned long first = 1, temp = 0, second, sum;
+
+	second = first + temp;
+	while (second <= 4000000)
+	{
+		if (second % 2 == 0)
+			sum += second;
+		temp = first;
+		first = second;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
