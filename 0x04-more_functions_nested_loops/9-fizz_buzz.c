6c6643b73281ab4324cab57132ad9871dc4d66a6
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-22 09:12:44 -0700

    solving the fizzbuzz task gleefully with c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - A fizzbuzz program to 100
+ *
+ * Return: 0 always
+ */
+
+int main(void);
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 && i % 5)
+			printf("FizzBuzz");
+		else if (i % 3)
+			printf("Fizz");
+		else if (i % 5)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+}
