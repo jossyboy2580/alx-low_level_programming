dafde929ac36d5eedebb789fe53dd202ffa9c814
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-26 16:35:42 -0700

    print only half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - This function prints the second half of a strimg
+ *
+ * @str: The string we want to print from
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	n = len / 2;
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+
+	for (n; n < len; n++)
+		_putchar(str[n]);
+	_putchar('\n');
+}
