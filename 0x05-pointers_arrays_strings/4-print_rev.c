bc78a4f7957c4c22a4f135e7ae2ae96e73edecc0
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-26 15:38:12 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+int _strlen(char *s);
+/**
+ * print_rev - A function that prints a string in reverse
+ *
+ * @s: the string to be peinted in reverse
+ */
+
+void print_rev(char *s)
+{
+	int strln = _strlen(s) - 1;
+
+	while (strln != 0)
+	{
+		_putchar(s[strln] + 0);
+		strln--;
+	}
+
+}
+
+/**
+ * _strlen - aget the length of the string
+ *
+ * @s: we need the length of this!
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
