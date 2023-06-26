4e183a5f4b68d1b5afdde4357efa8429ab71fbf7
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-26 16:09:25 -0700

    We really do have to copy this string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string with this function
+ *
+ * @s: The string we want to reverse
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char *rev;
+
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		*(rev + i) = s[len - (i + 1)]
+	}
+	for (i = 0; i < len; i++)
+		*(s + i) = rev[i];
+	
+}
