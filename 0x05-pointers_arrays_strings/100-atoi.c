6fc31645fb2f158e37e7a48adc18f64e4bcb8fad
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-27 19:15:39 +0000

    Atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _atoi - Function to convert the string to an integer
+ *
+ * @s: the string to convert
+ * Return: 1 if successful, 0 if otherwise
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	// Skip leading white space
+	while (s[i] == ' ' || s[i] == '\t' 
+}
