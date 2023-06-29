9b15ea88c3c717e3ae3253107d298b6c7e3024f9
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-29 14:36:07 -0700

    Copy one pointer to the other pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies n bytes from src
+ * to dest
+ *
+ * @dest: The destination of our copy operation
+ * @src: where we'll copy the bytes from
+ * @n: Number of bytes to copy
+ * Return: Pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (src[i] != '\0')
+			dest[i] = src[i];
+		else
+			dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
