b41660652d1e48d67623dec52fa220b9c44b6e5c
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-29 15:09:17 -0700

    Adjusted the strncpy program

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,11 +16,18 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (i < n)
 	{
-		if (src[i])
-			dest[i] = src[i];
+		if (src[i] == '\0')
+			break;
 		else
-			dest[i] = '\0';
+			dest[i] = src[i];
 		i++;
 	}
+	if (i < n)
+	{
+		for (; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	 }
 	return (dest);
 }
