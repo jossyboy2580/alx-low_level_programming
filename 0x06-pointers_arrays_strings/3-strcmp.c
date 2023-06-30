7624e31105ee5f3864aaf2e5687c08d12d064c56
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-30 10:10:33 -0700

    A comparison function debugging

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,8 @@
  * @s1: First string
  * @s2: Second string
  * Return: integer
- * 1 if s2 is greater than s -1 if s1 is greater than s2
+ * 1 if s2 is greater than s1
+ * -1 if s1 is greater than s2
  *  0 if they are equal
  */
 
@@ -18,12 +19,12 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0')
 	{
 		if (s1[i] > s2[i] || s2[i] == '\0')
-			return (1);
+			return (15);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (-15);
 		i++;
 	}
 	if (s2[i] != '\0')
-		return (-1);
+		return (-15);
 	return (0);
 }
