e01057c1a0f905597d8687d4e696d6fbac70f6e2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-30 15:18:17 -0700

    capitalize each word in a string with this function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+int is_separator(char c);
+/**
+ * cap_string - Capitalize every word in the string
+ *
+ * @s: The string to capitalize
+ * Return: pointer to the capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (is_separator(s[i]))
+		{
+			if (s[i + 1] <= 122 && s[i + 1] >= 97)
+				s[i + 1] = s[i + 1] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * is_separator - A functiom to check if given character
+ * is a separator
+ *
+ * @c: The character to check
+ * Return: 1 if its a separator and 0 if it is not
+ */
+
+int is_separator(char c)
+{
+	char *separators = ", ;.!?\"()}{";
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		if (c == separators[i])
+			return (1);
+	}
+	return (0);
+}
