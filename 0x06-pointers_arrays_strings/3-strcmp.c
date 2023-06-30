35e5e10982e1f2a8f8c61899eb12c474e1f6c671
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-30 01:26:25 -0700

    string compare adjustment

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,7 @@
  * @s1: First string
  * @s2: Second string
  * Return: integer
- * 1 if s2 is greater than s1
- * -1 if s1 is greater than s2
+ * 1 if s2 is greater than s -1 if s1 is greater than s2
  *  0 if they are equal
  */
 
@@ -19,12 +18,12 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0')
 	{
 		if (s1[i] > s2[i] || s2[i] == '\0')
-			return (15);
+			return (1);
 		else if (s1[i] < s2[i])
-			return (-15);
+			return (-1);
 		i++;
 	}
 	if (s2[i] != '\0')
-		return (-15);
+		return (-1);
 	return (0);
 }
