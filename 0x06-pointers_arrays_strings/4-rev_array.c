6ca0a61758a5dc5652067a7ff550c9f74357ae47
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-06-30 10:07:26 -0700

    reverse the loop program fixed with the help of Koffi Noumonvi on slack

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,13 +12,11 @@ void reverse_array(int *a, int n)
 	int i = 0, temp, middle;
 
 	middle = n / 2;
-	if (n % 2 != 0)
-		middle = (n - 1) / 2;
 	while (i < middle)
 	{
 		temp = a[i];
 		a[i] = a[(n - 1) - i];
-		a[n - i] = temp;
+		a[(n - 1) - i] = temp;
 		i++;
 	}
 }
