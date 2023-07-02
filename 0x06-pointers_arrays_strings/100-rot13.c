3c3aff231d4960da4eaa97d8f886c86590050f9b
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-02 09:21:58 +0000

    My rot13 code is behaving funny

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int get_image(int, int);
 /**
  * rot13 - A function that implements the rot13 encryption
  * algorithm
@@ -10,21 +11,32 @@
 
 char *rot13(char *s)
 {
-	int i = 0, normalized_value;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] <= 122 && s[i] >=97)
 		{
-			normalized_value = (s[i] - 97) % 26;
-			s[i] = normalized_value + 97;
+			s[i] = get_image(s[i], 122);
 		}
 		else if (s[i] <= 90 && s[i] >= 65)
 		{
-			normalized_value = (s[i] - 65) % 26;
-			s[i] = normalized_value + 65;
+			s[i] = get_image(s[i], 90);
 		}
 		i++;
 	}
 	return (s);
+}
+
+/**
+ * get_image - Function to get the rot13 image of num
+ *
+ * @num: The number whose image we seek
+ * @stop: the end of the range
+ * Return: an integer value of the map
+ */
+
+int get_image(int num, int stop)
+{
+	return (((num + 13) % stop) + (((num + 13) / stop) * 64));
 }
