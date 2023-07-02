7fd6098fe6bfd2d332047b20557f7c27d59de26e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-02 09:58:19 +0000

    an updated version of the rot13 algorithm

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,42 +1,28 @@
 #include "main.h"
 
-int get_image(int, int);
 /**
- * rot13 - A function that implements the rot13 encryption
- * algorithm
+ * rot13 - A function to implement the rot13 substitution cipher
  *
- * @s: The string we want to reverse
- * Return: A pointer to the rotated string
+ * @s: The string we want to map
+ * Return: the pointer to the string s
  */
 
-char *rot13(char *s)
+char *rot13(char *s);
 {
-	int i = 0;
+	char *domain = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *co_domain = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i]; i++)
 	{
-		if (s[i] <= 122 && s[i] >=97)
+		for (j = 0; domain[j]; j++)
 		{
-			s[i] = get_image(s[i], 122);
+			if (s[i] == domain[i])
+			{
+				s[i] = co_domain[i];
+				break;
+			}
 		}
-		else if (s[i] <= 90 && s[i] >= 65)
-		{
-			s[i] = get_image(s[i], 90);
-		}
-		i++;
 	}
 	return (s);
-}
-
-/**
- * get_image - Function to get the rot13 image of num
- *
- * @num: The number whose image we seek
- * @stop: the end of the range
- * Return: an integer value of the map
- */
-
-int get_image(int num, int stop)
-{
-	return (((num + 13) % stop) + (((num + 13) / stop) * 64));
 }
