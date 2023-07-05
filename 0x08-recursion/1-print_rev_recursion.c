ccddc11fddcc6dd15aa6b04e27ba05db8e5f61d0
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-05 07:30:14 -0700

    Reverse a string using recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+void reverse_printer(char *s, int len);
+/**
+ * _print_rev_recursion - Reverse a string and print
+ *
+ * @s: the string to print in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	reverse_printer(s, len - 1);
+}
+
+/**
+ * reverse_printer - A function to print a string in
+ * reverse
+ *
+ * @s: Pointer to a string
+ * @len: The last point
+ */
+
+void reverse_printer(char *s, int len)
+{
+	if (len <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[len] + 0);
+	reverse_printer(s, len - 1);
+}
