d762ffc08cd5be9b3e7a6ce7b83bc19ff7fcc5d5
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-05 15:52:10 -0700

    get the square root of a number

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+int check_palindrome(char *s, int start, int width);
+/**
+ * is_palidrome - A function to check if a string is a palidrome
+ *
+ * @s: A string to check if its palindrome
+ * Return: 1 if its palindrome 0 if its not
+ */
+
+int is_palindrome(char *s)
+{
+	int width = strlen(s);
+
+	if (width <= 1)
+		return (1);
+	return (check_palindrome(s, 0, width - 1));
+}
+
+/**
+ * check_palindrome - Is it palindrome
+ * @s: String to check
+ * @width: width of the string
+ * Return: 1 if palidrone 0 if not
+ */
+
+int check_palindrome(char *s, int start,int end)
+{
+	if ((start == end) || ((end - start) == 1))
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start++, end--));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int get_sqrt(int, int);
+/**
+ * _sqrt_recursion - Recursive determination of the square root
+ * of n
+ *
+ * @n: The number whose aquare root we seek
+ * Return: sqaure root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int half;
+
+	half = n / 2;
+	return (get_sqrt(n, half));
+}
+
+/**
+ * get_sqrt - Determine the square root of n
+ * @n: we find the sqrt of this number
+ * @half; midpoint of n
+ * Return: Sqrt of n
+ */
+
+int get_sqrt(int n, int half)
+{
+	if (half < 2)
+		return (-1);
+	if ((half * half) == n)
+		return (half);
+	return get_sqrt(n, half - 1);
+}
