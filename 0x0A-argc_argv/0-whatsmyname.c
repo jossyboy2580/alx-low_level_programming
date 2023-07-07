f2269f737479a0937158e8a5231bdf92c18ef177
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-07 12:05:59 -0700

    A program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program that prints its own name
+ *
+ * @argc: Number of arguments used to invoke the program
+ * @argv: An array of command line arguments used to call the 
+ * program
+ * Return: 0 to indicate successful execution
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
