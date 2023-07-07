1ccbf9cde910c05dc3a036d781e9cf14b61725a9
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-07 15:25:20 -0700

    infinity add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *  * main - A function to add n variable number of integer args
+ *   *
+ *    * @argc: the number of arguments
+ *     * @argv: Array of string arguments
+ *      * Return: 0 if successful and 1 if not
+ *       */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	if (argc > 1)
+	{
+		int count, digit;
+		for (count = 1; count < argc; count++)
+		{
+			char *arg = argv[count];
+			int i;
+			for (i = 0; arg[i] != '\0'; i++)
+			{
+				if (isdigit(arg[i]))
+				{
+					digit = arg[i] - '0';
+					sum += digit;
+				}
+				else
+				{
+					printf("Error\n");
+					return 1;
+				}
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	return 0;
+}
+
