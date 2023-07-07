2dcee250bfeab6f8c274d4aadc37a08834eb0a05
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-07 14:39:03 -0700

    A functin to multiply two arguments fed to the program

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,9 @@ int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		printf("%d\n", argv[1] * argv[2]);
+		int product = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", product);
 		return (0);
 	}
 	else
