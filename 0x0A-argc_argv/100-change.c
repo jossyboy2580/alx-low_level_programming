dc8791db6bb3c74afde2efaed2cd7d260f971b27
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-07-09 10:49:44 -0700

    Calculate changes with this program

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,23 @@
 #include <stdlib.h>
+#include <ctype.h>
 #include <stdio.h>
+
+/**
+ * is_digit - This function checks if a given argument is a digit
+ *
+ * @arg: argument to check
+ */
+
+int is_digit(char *s)
+{
+	while (*s != '\0')
+	{
+		if (isdigit(*s) == 0)
+			return (0);
+		s++;
+	}
+	return (1);
+}
 
 /**
  * main - A program that accepts an input and print the lowest
@@ -15,27 +33,30 @@ int main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		int val, index;
 		int cents[] = {25, 10, 5, 2, 1};
 
 		if (argv[1][0] == '-')
 		{
 			printf("0\n");
-			return (1);
+	
 		}
-		for (index = 0; argv[1][index] != '\0'; index++)
+		if (is_digit(argv[1]))
 		{
-			if (isdigit(argv[1][index]) == 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		val = atoi(argv[1]);
+			int val;
+			int i = 0, count = 0;
 
-		while (val > 0)
-		{
-			while (val >= present)
+			val = atoi(argv[1]);
+			while (val > 0)
+			{
+				int present = cents[i];
+
+				while (val > present)
+				{
+					val = val - present;
+					count++;
+				}
+				i++;
+			}
 		}
 	}
 	else
@@ -43,4 +64,5 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
