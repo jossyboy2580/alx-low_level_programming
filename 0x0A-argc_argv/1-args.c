e399fb274bb06b9733e597f613134b3a08ad71e6
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-04 04:38:27 -0700

    Print the number of commandline args used to invoke this program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program that prints the number of
+ * command line args used to invoke it
+ *
+ * @argc: Number of commandline args
+ * @argv: array of arguments
+ * Return: 0 always successsful
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d", argc);
+	return (0);
+}
