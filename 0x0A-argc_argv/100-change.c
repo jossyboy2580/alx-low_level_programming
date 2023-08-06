cbe9c5f711b5a9fe6a110a282acd8200588574a4
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-06 09:50:54 -0700

    Made some improvements to the change program

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+int is_digit(char *);
 /**
- * main - Entry point to a program that computes the change
- * in cents for an amount entered by the user
- *
- * @argc: Number of args used to call the program
- * @argv: Array of args used to call the program
- * Return: 0 always true
- *
+ *  * main - Entry point to a program that computes the change
+ *   * in cents for an amount entered by the user
+ *    *
+ *     * @argc: Number of args used to call the program
+ *      * @argv: Array of args used to call the program
+ *       * Return: 0 always true
+ *        *
  */
 
 int main(int argc, char **argv)
@@ -28,32 +30,46 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		int j = 0;
 		int arg_in_digit, current_cent;
 		unsigned long int idx = 0;
 
-		while (argv[1][j] != '\0')
+		if (is_digit(argv[1]))
 		{
-			if (argv[1][j] < 48 || argv[1][j] > 57)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			j++;
-		}
-		arg_in_digit = atoi(argv[1]);
+			arg_in_digit = atoi(argv[1]);
 
-		while (arg_in_digit > 0 && idx < sizeof(cents) / sizeof(cents[0]))
-		{
-			current_cent = cents[idx];
-			while (arg_in_digit >= current_cent)
+			while (arg_in_digit > 0)
 			{
-				count++;
-				arg_in_digit = arg_in_digit - current_cent;
+				current_cent = cents[idx];
+				while (arg_in_digit >= current_cent)
+				{
+					count++;
+					arg_in_digit = arg_in_digit - current_cent;
+				}
+				idx++;
 			}
-			idx++;
 		}
 	}
 	printf("%d\n", count);
 	return (0);
+}
+
+/**
+ * is_digit - An implementation of isdigit from the ctype
+ * library
+ *
+ * @num: a string we want to check
+ * Return: 1 if its a digit and 0 if its not
+ */
+
+int is_digit(char *num)
+{
+	int i = 0;
+
+	while (num[i] != '\0')
+	{
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
