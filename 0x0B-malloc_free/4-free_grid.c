d399ef68a17dff21726e1bcbea34207e4bae9f90
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-09 16:36:36 -0700

    Free some memories

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - A program to free some memory we already created
+ *
+ * @grid: The grid we want to free
+ * @height: The number of rows in the grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[height]);
+	}
+	free(grid);
+}
