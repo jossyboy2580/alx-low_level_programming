31bccb97b86ef806e6d2a503b1d5ce66301dd75a
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-09 15:53:27 -0700

    multidimensional array memory allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - A program that allocates mem for a multi
+ * dimensional grid and initialize ot to 0
+ *
+ * @width: hoe many columns the grid has
+ * @heigth: how many rows it has
+ * Return: a null or a pointer to the grid if sucvessful
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **new_array;
+	int i, j;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	new_array = malloc(sizeof(int) * width * height);
+	if (!new_array)
+		return (NULL);
+	for (i = 0; i < width * height; i++)
+	{
+		new_array[i] = 0;
+	}
+	return (new_array);
+}
