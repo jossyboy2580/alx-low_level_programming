f40eb9385dc1dff9f0d0cfd5fc842d76b6158c87
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-09 15:13:49 -0700

    A firat trial at the xoncatenate string task for the malloc free project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenate two strings
+ *
+ * @s1: First string
+ * @s2: Second string
+ * Return: Pointer to the new string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int len_s1 = 0, len_s2 = 0,len_both = 0;
+	char *new;
+
+	while (s1[len_s1] != '\0')
+		len_s1++;
+	while (s2[len_s2] != '\0')
+		len_s2++;
+	len_both = len_s1 + len_s2;
+	new = malloc(sizeof(char) * (len_both + 1));
+	if (len_s1 > 0)
+	{
+		for (i = 0; i < len_s1; i++)
+			new[i] = s1[i];
+	}
+	if (len_s2 > 0)
+	{
+		for (i = len_s1; i < len_both; i++)
+			new[i] = s[i - len_s1];
+	}
+	new[len_both] = '\0';
+	return (new);
+}
