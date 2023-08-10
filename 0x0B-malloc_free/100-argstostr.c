79b40ae1438092f240e87120ac21a667458f7c0c
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-09 22:02:28 -0700

    Something sumptious on the firat try for this prpgram that prints all its args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - A prpgram that concatenates a list of strings
+ *
+ * @ac: is the argument count
+ * @av: is the array of strings
+ * Return: A pointer to a new string or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int len_all, i, j, new_ind = 0;
+	char *new;
+	
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i])
+		{
+			int len_indiv = 0;
+
+			while (av[i][len_indiv] != '\0')
+				len_indiv++;
+			len_all += len_indiv;
+		}
+	}
+	new = malloc(sizeof(char) * (len_all + (ac * 1)));
+	if (!new)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		if (av[i])
+		{
+			for (j = 0; av[i][j] != '\0'; j++)
+			{
+				new[new_ind] = av[i][j];
+				new_ind++;
+			}
+			new[new_ind] = '\n';
+			new_ind++;
+		}
+	}
+	new[new_ind] = '\0';
+	return (new);
+}
