5e062252edb498d81b687f059a9fee8779348ce1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-11 03:58:59 -0700

    Implementing calloc with malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocate memory for an array of nmemb elements
+ * each with size size
+ *
+ * @nmemb: number of members
+ * @size the size of each
+ * Return: Pointer to the new mem or NULL if malloc fails
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *new_mem;
+	unsigned int i;
+
+	new_mem = malloc(nmemb * size);
+	if (!new_mem)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		new_mem[i] = 0;
+	return (new_mem);
+}
