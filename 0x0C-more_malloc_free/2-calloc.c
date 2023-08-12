53705e610ef1a9dc470b5498216d70c9b7ee21d1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-12 13:49:31 -0700

    Calloc redo

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,30 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc - Allocate memory for an array of nmemb elements
- * each with size size
- *
- * @nmemb: number of members
- * @size: the size of each
- * Return: Pointer to the new mem or NULL if malloc fails
+ *  * _calloc - Allocate memory for an array of nmemb elements
+ *   * each with size size
+ *    *
+ *     * @nmemb: number of members
+ *      * @size: the size of each
+ *       * Return: Pointer to the new mem or NULL if malloc fails
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *new_mem;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
+
 	new_mem = malloc(nmemb * size);
 	if (!new_mem)
 		return (NULL);
+
 	for (i = 0; i < nmemb; i++)
-		new_mem + i = 0;
+		*((char *)new_mem + i * size) = 0;
+
 	return (new_mem);
 }
+
