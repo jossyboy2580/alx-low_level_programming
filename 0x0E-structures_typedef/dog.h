cd7edc11edfe7c2a3b57db8fae6c6b1cb298baf6
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-14 13:14:11 -0700

    New struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,11 @@
  * @age: How old is the dog
  * @owner: Who owns the dog
  */
-struct dog {
+typedef struct dog {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
