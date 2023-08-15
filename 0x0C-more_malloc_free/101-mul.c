2ffe2e193f2aabfa784daca79f4bb0f597f67117
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-15 04:12:36 -0700

    Multiplier

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include <stdio.h>
+
+
+
 
 /**
  * is_digit - A function to chexk if a string is made up of
@@ -21,12 +25,47 @@ int is_digit(char *str)
 	return (1);
 }
 
-int *str_to_int_arr_inverse(char *str)
-{
-	int *new_mem;
-	unsigned int len_str;
+/**
+ * str_to_int_arr - A function that converts a string made up of
+ * integers to an array of integers
+ *
+ * @str: The string we want to convert
+ * @len: The length of the string
+ * Return: An array of integers if the operation is successful
+ * and NULL if not
+ */
 
-	len_str = strlen(str);
+int *str_to_int_arr(char *str, int len)
+{
+	int *num_array;
+	int i;
+
+	num_array = malloc(sizeof(int) * len);
+	if (!num_array)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		num_array[i] = atoi(str[i]);
+	}
+	return (num_array);
+}
+
+void rev_array(int *arr)
+{
+	unsigned int i, len;
+	int temp;
+
+	if (arr)
+	{
+		len = sizeof(arr) / sizeof(int);
+
+		for (i = 0; i < len / 2; i++)
+		{
+			temp = arr[i];
+			arr[len - i - 1] = arr[i];
+			arr[i] = temp;
+		}
+	}
 }
 /**
  * main - The entry point of a program that multiplies two numbers
@@ -38,6 +77,10 @@ int *str_to_int_arr_inverse(char *str)
 
 int main(int argc, char* argv[])
 {
+	int *first_arr;
+	int *second_arr;
+	int i;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -48,5 +91,11 @@ int main(int argc, char* argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	
+	first_arr = str_to_int_arr(argv[1], strlen(argv[1]));
+	second_arr = str_to_int_arr(argv[2], strlen(argv[2]));
+	printf("Array 1\n");
+	printf("--------------------------\n");
+	for (i = 0; i < strlen(argv[1]); i++)
+		printf("%d", first_arr[i]);
+	printf("\n\n-------------++++++++--------------\n\n");
 }
