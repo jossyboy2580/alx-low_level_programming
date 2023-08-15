5b31cb4d391ae0df87fe7981d5b5aee98f1ca46e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-15 08:29:06 -0700

    A new dog was born

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,36 +13,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *name_cpy;
-	char *owner_cpy;
-	dog_t *a_dog;
-	int i;
+	dog_t a_dog;
 
-	name_cpy = malloc(sizeof(char) * strlen(name));
-	if (name_cpy == NULL)
-		return (NULL);
-	for (i = 0; name[i] != '\0'; i++)
-		name_cpy[i] = name[i];
-	name_cpy[strlen(name)] = '\0';
-	owner_cpy = malloc(sizeof(char) * strlen(owner));
-	if (owner_cpy == NULL)
-	{
-		free(name_cpy);
-		return (NULL);
-	}
-	for (i = 0; owner[i] != '\0'; i++)
-		owner_cpy[i] = owner[i];
-	owner_cpy[strlen(owner)] = '\0';
 	a_dog = malloc(sizeof(dog_t));
 	if (!a_dog)
+		return (NULL);
+	a_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (!a_dog->name)
 	{
-		free(name_cpy);
-		free(owner_cpy);
 		free(a_dog);
 		return (NULL);
 	}
-	a_dog->name = name_cpy;
+	strcpy(a_dog->name, name);
 	a_dog->age = age;
-	a_dog->owner = owner_cpy;
+	a_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (!a_dog->owner)
+	{
+		free(a_dog->name);
+		free(a_dog);
+		return (NULL);
+	}
+	strcpy(a_dog->owner, owner);
 	return (a_dog);
 }
