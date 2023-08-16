77e68d25fc90308ffa022576a7a80386ab443ab4
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-16 14:38:34 -0700

    Array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Iterate through an array and call a fn
+ * on each element
+ *
+ * @array: An array
+ * @size: The number of elements in the array
+ * @action: Pointer to the function we need to use
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
