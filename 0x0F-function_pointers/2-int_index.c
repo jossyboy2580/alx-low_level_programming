5d70119781788b6124a6a74e23cb57685497cd64
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-17 01:06:07 -0700

    A simple search algorithm

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - A program that searches for an integer
+ *
+ * @array: The array we want to search
+ * @size: The number of elem in the array
+ * @cmp: The comparison function
+ * Return: The position of the searched element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(i))
+				return (i);
+		}
+	}
+	return (-1);
+}
