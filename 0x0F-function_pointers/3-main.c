8bb5e0ac08a171a1305d18df5892c684a0ca0a52
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-23 04:33:09 -0700

    Now it works

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,19 +11,26 @@
 int main(int argc, char *argv[])
 {
 	int (*op)(int, int);
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
+	int a;
+	int b;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	if (strlen(argv[2]) != 1 || argv[2] == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	return (op(a, b));
 }
