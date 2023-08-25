2a02f4463bdbfe3e249c4d82595400b05ba67117
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-25 13:44:52 -0700

    added soome libes

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,17 @@
 section .data
-    format db "Hello, %s",0xa,0
-    name db "Holberton",0
+format db "Hello, %s",0xa,0
+name db "Holberton",0
 
 section .text
-    global main
-    extern printf
+global main
+extern printf
 
 main:
-    sub rsp, 8               ; Align stack to 16 bytes
-    lea rdi, [format]        ; Load format string address into rdi
-    lea rsi, [name]          ; Load name string address into rsi
-    call printf             ; Call printf
-    add rsp, 8               ; Restore stack alignment
-    ret
-
+sub rsp, 8               ; Align stack to 16 bytes
+lea rdi, [format]        ; Load format string address into rdi
+lea rsi, [name]          ; Load name string address into rsi
+call printf             ; Call printf
+add rsp, 8               ; Restore stack alignment
+mov rax, 60              ; Syscall number for exit = 60
+xor rdi, rdi             ; Exit status 0 for success
+syscall
