41b529f9567877b3cb1311531675ab4bd7d787b0
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-25 13:30:57 -0700

    hmmmmm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,16 @@
 section .data
-format db "Hello, %s",0xa,0
-name db "Holberton",0
+    format db "Hello, %s",0xa,0
+    name db "Holberton",0
 
 section .text
-global main
-extern printf
+    global main
+    extern printf
 
 main:
-sub rsp, 8               ; Align stack to 16 bytes
-lea rdi, [format]        ; Load format string address into rdi
-lea rsi, [name]          ; Load name string address into rsi
-call printf             ; Call printf
-add rsp, 8               ; Restore stack alignment
-
-; Call exit system call
-mov rax, 60             ; Syscall number for exit = 60
-xor rdi, rdi            ; Exit status (0)
-syscall
-
-section .text
-global exit           ; Define a label for exit
-exit:
-mov rax, 60             ; Syscall number for exit = 60
-xor rdi, rdi            ; Exit status (0)
-syscall
+    sub rsp, 8               ; Align stack to 16 bytes
+    lea rdi, [format]        ; Load format string address into rdi
+    lea rsi, [name]          ; Load name string address into rsi
+    call printf             ; Call printf
+    add rsp, 8               ; Restore stack alignment
+    ret
 
