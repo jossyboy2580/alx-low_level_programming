8e6fec4386dccd2bdae9fd45cfe500ff61a30e17
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-28 20:20:51 -0700

    Pop the head off of a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Pop the head of a listint
+ *
+ * @head: The head of the list
+ * Return: the value of the head
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *dummy_head;
+
+	dummy_head = *head;
+	*head = dummy_head->next;
+	n = dummy_head->n;
+	free(dummy_head);
+	return (n);
+}
