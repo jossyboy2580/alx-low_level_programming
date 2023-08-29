0abfd07e9ae123e0e4817fcf3c984f8383b53a01
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-28 19:40:02 -0700

    Free list but set the head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - A functiin that frees a lost but sets the head to
+ * null
+ *
+ * @head: The head of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new_head;
+	listint_t *next;
+
+	new_head = head->->next;
+	*head = NULL;
+	while (new_head)
+	{
+		next = new_head->next;
+		free(new_head);
+		new_head = next;
+	}
+}
