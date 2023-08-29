027efba226efd7263e74f8bc241263391701138e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-08-29 15:04:54 -0700

    Get the node at the index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index Get the node at the index with this func
+ *
+ * @head: The head of the list
+ * @index: The index we want to fetch
+ * Return: The node at the index
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (*head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
