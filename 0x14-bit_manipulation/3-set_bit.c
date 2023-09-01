a3a354f2a31475f7054f64f5b8347cd92fcd9ee2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-01 10:43:12 -0700

    Adjust a particular bit to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - This function sets the valie of a bit to 1
+ *
+ * @n: The number we want to change
+ * @index: the index we want to manipulate
+ * Return: 1 if it worked and -1 if it didnt
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int shifted;
+
+	shifted = 1;
+	shifted <<= index;
+	n = n | shifted;
+	return (1);
+}
