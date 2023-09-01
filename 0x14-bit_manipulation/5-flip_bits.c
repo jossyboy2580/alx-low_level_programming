b9bf10454444d35ea81e9cd64befb3429c89806e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-01 14:33:19 -0700

    Get the number of flips to make

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * flib_bits - Number of bits we need to flip to get one num from
+ * thw other
+ *
+ * @m: The first number
+ * @b: The second number
+ * Return: The number of bits we need to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int flips = 0;
+	unsigned long int mask;
+
+	mask = 1;
+
+	while (n != 0 || m != 0)
+	{
+		if (n & mask == m & mask)
+			flips++;
+		n >>= 1;
+		m >>= 1;
+	}
+	return (flips);
+}
