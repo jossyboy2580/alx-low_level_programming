649efe42fbeefd95282b47391ab3003101cbefa1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-01 16:55:18 -0700

    Set the bits up

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,6 +14,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (n == NULL)
 		return (-1);
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 	shifted = 1;
 	shifted <<= index;
 	*n = *n | shifted;
