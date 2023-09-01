7068a5f548012d81cffd6755bc64edba41ad2478
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-01 16:57:18 -0700

    clear some bits

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,6 +12,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int shift;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 	shift = 1;
 	shift <<= index;
 	shift = ~shift;
