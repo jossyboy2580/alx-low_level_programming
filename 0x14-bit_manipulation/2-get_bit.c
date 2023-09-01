7044b23d75efef0d9372f58074edcebe142385c9
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-01 16:50:50 -0700

    Get the bit at index updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,12 +14,13 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int n_cpy;
 
 	mask = 1;
-	if ((n_cpy >> index) != 0)
+	if (index >= sizeof(unsigned long int) * 8)
 	{
-		if (((n >> index) & mask) == 1UL)
-			return (1);
-		else
-			return (0);
+		return (-1);
 	}
+	if (((n >> index) & mask) == 1UL)
+		return (1);
+	else if (((n >> index) & mask) == 0UL)
+		return (0);
 	return (-1);
 }
