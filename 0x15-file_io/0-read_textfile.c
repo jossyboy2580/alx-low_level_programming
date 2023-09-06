f4a5586bbd5856d81837b721d792a6166e112056
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-09-05 17:27:03 -0700

    Read so.e files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,25 @@
 #include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
 
 /**
- * read_textfile - A function to read text from a file
- *
- * @filename: The name of the file we wish to read
- * @letters: The number of letters we are to read
- * Return: the number of written letters
- */
+ *  * read_textfile - A function to read text from a file
+ *   *
+ *    * @filename: The name of the file we wish to read
+ *     * @letters: The number of letters we are to read
+ *      * Return: the number of written letters
+ *       */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int my_file = open(filename, O_RDONLY);
-	char *words;
+	char buffer[1024];
+	ssize_t bytes_read;
 
-	words = malloc(sizeof(char) * (letters + 1));
-	if (words == NULL)
-		return (-1);
-	read(my_file, words, letters);
-	return (write(1, words, letters));
+	if (my_file == -1)
+	{
+		return -1;
+	}
+	bytes_read = read(my_file, buffer, letters);
+	buffer[bytes_read + 1] = '\0';
+	return (write(1, buffer, bytes_read));
 }
