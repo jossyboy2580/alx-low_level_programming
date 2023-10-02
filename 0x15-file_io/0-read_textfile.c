b24b0b7dfff68931a243298945c8a26fdd415521
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-10-02 05:03:29 -0700

    Lets read a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (read_count != -1)
 		{
 			write_count = write(1, reading, read_count);
-			if (write_count != read_count)
+			if (write_count != read_count || write_count == -1)
 				return (0);
 		}
 	}
