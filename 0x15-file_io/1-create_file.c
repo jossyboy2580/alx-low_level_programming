36d535522eeb6a38194fd1313aa9dada7a5a3aa4
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-10-02 15:51:52 -0700

    Write some text to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_des;
-	ssize_t write_count;
 
 	if (filename == NULL)
 		return (-1);
-	file_des = open(filename, O_CREATE|O_TRUNC, 0600);
+	file_des = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
 	if (file_des == -1)
 	{
 		return (-1);
 	}
 	if (text_content != NULL)
 	{
-		write_count = write(file_des, text_content, strlen(text_content));
+		write(file_des, text_content, strlen(text_content));
 	}
+	close(file_des);
+	return (1);
 }
