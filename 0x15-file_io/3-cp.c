f57541d8739ac95e7d784a5b421ed5303bb82371
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-10-02 17:11:45 -0700

    Copying some files to the other

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int move_file(char *src, char *dest)
 {
 	int src_fd, dest_fd;
 	char buf[1024];
-	ssize_t read_count, write_count;
+	ssize_t read_count;
 
 	src_fd = open(src, O_RDONLY);
 	if (src_fd == -1)
@@ -13,11 +14,18 @@ int move_file(char *src, char *dest)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
-	dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	dest_fd = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0466);
 	if (dest_fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
 	}
+	do
+	{
+		read_count = read(src_fd, buf, 1024);
+		write(dest_fd, buf, read_count);
+	}
+	while (read_count >= 1024);
+	return (1);
 }
 
 /**
@@ -29,10 +37,15 @@ int move_file(char *src, char *dest)
 
 int main(int ac, char **av)
 {
+	int moved;
+
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	moved = move_file(av[1], av[2]);
+	if (!moved)
+		return (-1);
+	return (1);
 }
