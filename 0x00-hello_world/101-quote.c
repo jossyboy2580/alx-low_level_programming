d693d1d5eeb59a1a8d2df0a9b6d8d7eabc420fe1
Ajobiewe Joseph <joseph4hacking@gmail.com>
2023-12-15 04:08:46 +0100

    Removed some space and broke the write line into two, hopefully it works this time

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,7 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59 );
+	write(2, "and that piece of art is useful", 31);
+	write(2, "\" - Dora Korpar, 2015-10-19\n", 28);
 	return (1);
 }
