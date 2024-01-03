02397d77690c9f56b6ff29e432ca89256ae31c54
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-01-03 02:44:13 +0100

    Removed the trailing white space

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,22 @@
-#include "main.h" 
+#include "main.h"
 
 /**
- * _atoi - A function to try out an alternative technique for converting a string to an
- * integer
+ * _atoi - A function to try out an alternative technique for
+ * converting a string to an integer
  *
- * @s: The string that contains the set of numbers we want to convert to an integer
+ * @s: The string that contains the set of numbers
+ * we want to convert to an integer
  * Return: An integer representation of the string
  */
 
 int _atoi(char *s)
 {
-	int len = 0;
-	int seen_space = 0;
-	int result = 0;
-	int multiplier = 1;
+	int len = 0, seen_space = 0, result = 0, multiplier = 1;
 
-	/* Get the length of the string */
 	while (s[len] != '\0')
 		len++;
-	/* Reduce the length by one to enable us index the last element */
 	len--;
 
-	/* Start looping from the back and add up numbers to the result*/
 	while (len > -1)
 	{
 		if (s[len] == '-')
