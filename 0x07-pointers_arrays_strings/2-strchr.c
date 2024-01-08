8f428c8ec470f26115ed13eb16a57ee1eef3d00f
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-01-08 17:29:47 +0100

    Lets locate a character in the string and return the first occurence

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,14 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-
+	
+	if (s == NULL)
+		return (NULL);
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
