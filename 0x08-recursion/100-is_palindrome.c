225de90594e23fb58828a570f3faaeb87129a507
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-01-10 23:13:20 +0100

    A palindrome checker designed with the use of recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+int check_pal(char *start, char *end)
+{
+	if (start == end)
+		return (1);
+	if (*start == *end)
+	{
+		if (end - start == 1)
+			return (1);
+		return (check_pal((start + 1), (end - 1)));
+	}
+	return (0);
+}
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+	int pal;
+
+	if (s == NULL)
+		return (0);
+	return (check_pal(s, (s + (len - 1))));
+}
