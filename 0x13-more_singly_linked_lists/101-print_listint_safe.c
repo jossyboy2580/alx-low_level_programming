9baa61f920139cccbf21059aba2ec816ceda9be2
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-02-06 16:48:45 -0800

    Print a linked list while looking out for a loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
-int is_visited(listint_t **visited, listint_t *head, size_t count)
+int is_visited(listint_t **visited, listint_t *head, size_t *count)
 {
 	size_t i;
 
 	i = 0;
-	while (i < count)
+	while (i < *count)
 	{
 		if (visited[i] == head)
 			return (1);
@@ -28,6 +28,17 @@ void add_visited(listint_t ***visited, listint_t *head, size_t count)
 	(*visited)[count - 1] = head;
 }
 
+size_t print_recurse(listint_t *head, listint_t ***visited, size_t *count)
+{
+	if (head == NULL)
+		return (*count);
+	if (is_visited(*visited, head, count))
+		return (*count);
+	(*count)++;
+	printf("[%p] %d\n", (void *)head, head->n);
+	add_visited(visited, head, *count);
+	return (print_recurse(head->next, visited, count));
+}
 
 size_t print_listint_safe(const listint_t *head)
 {
@@ -35,14 +46,5 @@ size_t print_listint_safe(const listint_t *head)
 	size_t printed = 0;
 	listint_t *new_head = (listint_t *)head;
 
-	while (new_head)
-	{
-		printed++;
-		if (is_visited(visited, new_head, printed))
-			break;
-		printf("[%p] %d", (void *)new_head, new_head->n);
-		add_visited(&visited, new_head, printed);
-		new_head = new_head->next;
-	}
-	return (printed);
+	return (print_recurse(new_head, &visited, &printed));
 }
