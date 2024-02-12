16adb928d63c755467cc953cbadd4a585e67d59e
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-02-12 23:40:33 +0100

    Read a certain amount of letters from a file and then print it to the standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - A function to read a certain number of letters from a file
+ *
+ * @filename: The file we want to read from
+ * @letters: The number of letters we want to read
+ * Return: The number of letters actually read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_des;
+	ssize_t read_count;
+	ssize_t write_count;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	buffer = malloc(sizeof(char) * letters + 1);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	file_des = open(filename, O_RDONLY);
+	if (file_des == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	read_count = read(file_des, buffer, letters);
+	if (read_count == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	write_count = write(1, buffer, read_count);
+	if (write_count == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	return (write_count);
+}
