1acf635acb028e587680fe7c4ab4c563067d3a19
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-02-13 23:14:35 +0100

    The copy program was complaining of too many lines in a single function, so i took a portion of the code and put them in a separate function that returns void

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,37 @@
 #define BUFF_SIZE 1024
 
 /**
+ * transfer_files - A function to transfer files between two
+ * open file descriptors
+ *
+ * @buffer: The holder for the stream of text we want to
+ * transfer
+ * @file_from: File descriptor for the source of the content
+ * @file_to: File descriptor for the destination of the content
+ * @argv: List of strings holding the name of the source and destination
+ */
+
+void transfer_files(char *buffer, int file_from, int file_to, char **argv)
+{
+	ssize_t read_count, write_count;
+
+	do {
+		read_count = read(file_from, buffer, BUFF_SIZE);
+		if (read_count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_count = write(file_to, buffer, read_count);
+		if (write_count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	} while (read_count >= 1024);
+}
+
+/**
  * main - The entry point of a program that copies the content
  * of one file to another
  *
@@ -15,50 +46,36 @@ int main(int argc, char **argv)
 {
 	int file_from, file_to;
 	int closed_read, closed_write;
-	ssize_t read_count, write_count;
-	char *buffer[BUFF_SIZE];
+	char buffer[BUFF_SIZE];
 
 	if (argc != 3)
 	{
-		write(2, "Usage: cp file_from file_to\n", 200);
+		write(2, "Usage: cp file_from file_to\n", 28);
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	do {
-		read_count = read(file_from, buffer, BUFF_SIZE);
-		if (read_count == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		write_count = write(file_to, buffer, read_count);
-		if (write_count == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	} while (read_count >= 1024);
+	transfer_files(buffer, file_from, file_to, argv);
 	closed_read = close(file_from);
 	if (closed_read == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	closed_write = close(file_to);
 	if (closed_write == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
