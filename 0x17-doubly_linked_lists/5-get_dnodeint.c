da9b90ca11e3266bd3689436970f975620ebc7a5
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-23 03:01:31 +0100

    Get a specific node in a liked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Get the node of the linked list at an index
+ * @head: The head of the linked list
+ * @index: The position of the node we are looking for
+ * Return: The pointer to the node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int index_count = 0;
+	dlistint_t return_node = NULL;
+
+	while (head && head->prev != NULL)
+		head = head->prev;
+
+	while (head && index_count <= index)
+	{
+		index_count++;
+		head = head->next;
+	}
+	return (head->prev);
+}
