f393ffec85dbb532033d5ea51d272d0efc167347
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-03-23 01:53:24 +0100

    How many elements are in this lists, well lets find out

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - A function to print the length of a doubly linked list
+ * @h: The head of the list
+ * Return: Number of elements in the list
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
