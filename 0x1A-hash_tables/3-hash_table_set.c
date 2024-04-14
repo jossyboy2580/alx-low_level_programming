80e1e04ec0e77e133b78750cfbc5c955f383e01c
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-14 00:41:48 -0700

    Didnt know betty worked with header files as well, fixed a betty compliance issue with the struct definitions

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - A fuction to place a key/value pair in a
+ * hash table
+ *
+ * @ht: The pointer to the hash table
+ * @key: the insertion key
+ * @value: The value associated with the key
+ *
+ * Return: status of the operation, 1 for successful
+ * 0 for otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	if (!ht)
+
+}
