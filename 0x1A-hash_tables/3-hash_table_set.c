d2a57561c39d1f8502da3c4c6a34e3d63bf7ee52
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-04-18 15:01:31 -0700

    Set hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,27 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node;
+	int key_ind;
 
 	if (!ht)
-
+		return (0);
+	if (!key || strlen(key) < 1)
+		return (0);
+	/* Make a new node */
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = NULL;
+	/* Get the index it should take */
+	key_ind = key_index((const unsigned char *)key, ht->size);
+	if (!ht->array[key_ind])
+		ht->array[key_ind] = node;
+	else
+	{
+		node->next = ht->array[key_ind];
+		ht->array[key_ind] = node;
+	}
+	return (1);
 }
